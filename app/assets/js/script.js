65e506a4cf7f0fdd13de414ca9306a347baa0d8e
nicolehoefler2018 <nicolehoefler2018@gmail.com>
2021-02-08 22:23:28 +0100

    Function add task points

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,2 +1,9 @@
-var message = "test";
-console.log(message);
+var add_btn = document.getElementById('add-button');
+var empty_place = document.getElementById('your-tasks');
+var checkbox = document.getElementById('checkbox-col');
+add_btn.addEventListener('click', function () {
+    var input_field_value = document.getElementById('task-field').value;
+    checkbox.innerHTML += '<input type="checkbox" name="checked" id="checked"><br>';
+    empty_place.innerHTML += '<p>' + input_field_value + '<br>' + '</p>';
+});
+//# sourceMappingURL=script.js.map
--- app/assets/js/script.ts
+++ app/assets/js/script.ts
@@ -1,2 +1,10 @@
-let message: string = "test";
-console.log(message);
+var add_btn: HTMLElement = document.getElementById('add-button');
+var empty_place: HTMLElement = document.getElementById('your-tasks');
+var checkbox: HTMLElement = document.getElementById('checkbox-col');
+
+add_btn.addEventListener('click', function(){
+    var input_field_value = (document.getElementById('task-field') as HTMLInputElement).value;    
+    checkbox.innerHTML += '<input type="checkbox" name="checked" id="checked">';
+    empty_place.innerHTML += '<p>'+input_field_value+'<br>'+'</p>'+'';
+
+})
