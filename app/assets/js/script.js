e1b9fa1a745cb3399bf38297498fb640cf0b2bf8
nicolehoefler2018 <nicolehoefler2018@gmail.com>
2021-02-12 21:53:45 +0100

    Delete Function V1

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,22 +1,34 @@
 var add_btn = document.getElementById('add-button');
 var empty_place = document.getElementById('your-tasks');
 var checkbox = document.getElementById('checkbox-col');
-var alt = document.getElementsByClassName("btn-edit");
+var complete_task_row;
 add_btn.addEventListener('click', function () {
     var input_field_value = document.getElementById('task-field').value;
-    empty_place.innerHTML += '<div class="row task-row justify-content-center">' +
+    empty_place.innerHTML += '<div class="row task-row justify-content-center" id="' + input_field_value + '">' +
         '<input type="checkbox" name="checked" id="checked">' +
         '<label class="input-p">' + input_field_value + '</label>' +
-        '<a class="btn btn-edit">Delete <i class="fa fa-trash-o"></i></a>' +
+        '<button onclick="removeDocument(\'' + input_field_value + '\')" class="btn btn-edit">Delete <i class="fa fa-trash-o"></i></button>' +
         '</div>';
-    var delete_buttons = document.querySelectorAll(".btn-edit");
-    for (var i = 0; i < delete_buttons.length; i++) {
-        function set_function(index) {
-            delete_buttons[index].addEventListener('click', function () {
-                console.log('works again');
-            });
-        }
-        set_function(i);
-    }
 });
+function removeDocument(text) {
+    var current_row = document.getElementById(text);
+    current_row.style.display = 'none';
+}
+// for(let i: number = 0; i < delete_buttons.length; i++){
+// console.log("ji");
+//     function set_function(index: number){
+//         delete_buttons[index].addEventListener('click', function(){
+//             function removeDocument(){
+//                 if (index > -1){
+//                     delete_buttons[index].remove;                    
+//                     complete_task_row[index].innerHTML = '';
+//                 }
+//             }                    
+//             console.log(index);
+//             console.log(delete_buttons.length);
+//             removeDocument();
+//         });
+//     }
+//     set_function(i);
+// }
 //# sourceMappingURL=script.js.map
